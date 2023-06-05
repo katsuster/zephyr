4c55196b0e3a991b2fff3046deda4f664f49de0a
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2023-06-05 10:41:20 +0900

    soc: riscv: riscv-privilege: Add support for NSITEXE NS31A SoC
    Add support for NSITEXE Akaria NS31/NS31A series SoC.
    
    Signed-off-by: Katsuhiro Suzuki <katsuhiro@katsuster.net>

---
+++ soc/riscv/nsitexe_akaria/ns31a/soc.h
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __RISCV_NSITEXE_AKARIA_NS31A_SOC_H_
+#define __RISCV_NSITEXE_AKARIA_NS31A_SOC_H_
+
+#endif /* __RISCV_NSITEXE_AKARIA_NS31A_SOC_H_ */
